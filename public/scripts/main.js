7a2dd2cec4d2a3b277084ffa2b73be7df1125547
Ruben Osorio <rubendaosorio@gmail.com>
2021-03-23 18:20:54 -0400

    adding messenger connectivity

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,22 @@
 (() => {
     console.log('fired');
+
+    // load the socket library and make a connnection
+    const socket = io();
+
+    const vm = new Vue ({
+        data: {
+            messages: [],
+            nickname: "",
+            username: ""
+        },
+
+        created: function() {
+            console.log('its alive!!');
+        },
+
+        methods: {
+
+        }    
+    }) .$mount("#app");
 })();
