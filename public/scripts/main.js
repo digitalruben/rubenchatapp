1756cc2e2b5928cdf526179e9ab3df734fe6ec7d
Ruben Osorio <rubendaosorio@gmail.com>
2021-03-24 00:02:14 -0400

    messages working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,30 @@
+import ChatMessage from "./components/TheMessageComponent.js"
+
 (() => {
     console.log('fired');
 
     // load the socket library and make a connnection
     const socket = io();
 
-    const vm = new Vue ({
+    // messenger service event handling - > incoming from the manager
+    function setUserId({sID, message}) {
+        // incominng connected event with data
+       // debugger;
+        vm.socketID = sID; 
+    }
+
+    function appendMessage(message) {
+        // debuger;
+        vm.messages.push(message);
+    }
+
+    const vm = new Vue({
         data: {
             messages: [],
             nickname: "",
-            username: ""
+            username: "",
+            socketID: "",
+            message: ""
         },
 
         created: function() {
@@ -16,7 +32,20 @@
         },
 
         methods: {
+            dispatchMessage() {
+                //debuger;
+                socket.emit('chatmessage', { content: this.message, name: this.nickname || "Anonimous" });
 
-        }    
+                this.message = "";
+            }
+
+        },    
+
+        components: {
+            newmessage: ChatMessage
+        }
     }) .$mount("#app");
+
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener("message", appendMessage);
 })();
